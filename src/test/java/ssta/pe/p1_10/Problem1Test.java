89432963cf03640bf258f03cb6398b0c0daefdc5
Stephen Stafford <clothcat@gmail.com>
2015-05-03 19:35:07 +0100

    Solutions and tests for Problems 3 and 4

--- src/test/java/ssta/pe/p1_10/Problem1Test.java
+++ src/test/java/ssta/pe/p1_10/Problem1Test.java
@@ -1,5 +1,6 @@
 package ssta.pe.p1_10;
 
+import com.google.common.base.Stopwatch;
 import junit.framework.TestCase;
 
 /**
@@ -7,7 +8,13 @@ import junit.framework.TestCase;
  */
 public class Problem1Test extends TestCase {
 
-    public void testSolve() throws Exception {
-        assertEquals("233168", new Problem1().solve());
-    }
+  public void testSolve() throws Exception {
+    String name = Problem1Test.class.getName();
+    System.out.println(name + " starting");
+    Stopwatch timer = Stopwatch.createStarted();
+    String result = new Problem1().solve();
+    assertEquals("233168", result);
+    timer.stop();
+    System.out.println(name + " took: " + timer);
+  }
 }
