65df6c2def360d1446ace72a9e96de99d36379e6
ssta <Stephen.Stafford@hpdsoftware.com>
2016-03-01 14:42:22 +0000

    P78

--- src/main/java/ssta/pe/p71_p80/Problem77.java
+++ src/main/java/ssta/pe/p71_p80/Problem77.java
@@ -1,60 +1,48 @@
 package ssta.pe.p71_p80;
 
-import ssta.pelib.PrimesFromFile;
+import ssta.pelib.SimplePrimeSieve;
 
-import java.io.IOException;
-import java.util.Arrays;
 import java.util.List;
 
 /**
- * The arithmetic sequence, 1487, 4817, 8147, in which each of the terms
- * increases by 3330, is unusual in two ways: (i) each of the three terms are
- * prime, and, (ii) each of the 4-digit numbers are permutations of one another.
- * <p>
- * There are no arithmetic sequences made up of three 1-, 2-, or 3-digit primes,
- * exhibiting this property, but there is one other 4-digit increasing sequence.
- * <p>
- * What 12-digit number do you form by concatenating the three terms in this
- * sequence?
+
+
+ It is possible to write ten as the sum of primes in exactly five different ways:
+
+ 7 + 3
+ 5 + 5
+ 5 + 3 + 2
+ 3 + 3 + 2 + 2
+ 2 + 2 + 2 + 2 + 2
+
+ What is the first value which can be written as the sum of primes in over five thousand different ways?
+
  */
 public class Problem77 {
 
-  List<Integer> primes = PrimesFromFile.getIntegerPrimes();
 
   public static void main(String[] args) throws Exception {
     Problem77 p = new Problem77();
     System.out.println(p.solve());
   }
 
-  private String sortDigits(int n) {
-    char[] chars = String.valueOf(n).toCharArray();
-    Arrays.sort(chars);
-    return new String(chars);
-  }
+  public String solve() {
+    List<Long> p = SimplePrimeSieve.getSieveWithMaxPrime(500).getPrimes();
+    int[] primes = new int[p.size()];
+    for (int i = 0; i < p.size(); i++) { primes[i] = p.get(i).intValue(); }
 
-
-  private boolean isPrime(int n) {
-    return primes.contains(n);
-  }
-
-  public String solve() throws IOException, InterruptedException {
-    // Turns out that the answer also is increasing by 3330 (weird, but true)
-    // which allows for a significant optimisation...
-
-    for (int p : primes) {
-      // only want to look at 4 digit primes.
-      if (p < 1000) {continue;}
-      // we don't want to repeat the one the question gives us...
-      if (p == 1487) {continue;}
-      if (isPrime(p + 3330) && isPrime(p + 6660)) {
-        if (sortDigits(p).equals(sortDigits(p + 3330)) &&
-            sortDigits(p).equals(sortDigits(p + 6660)))
-          //System.out.println("Found : " + p);
-          return String.valueOf(p).concat(String.valueOf(p + 3330)).concat(String
-              .valueOf(p + 6660));
+    int x = 2;
+    while (true) {
+      int[] ways = new int[x + 1];
+      ways[0] = 1;
+      for (int i = 0; i < primes.length; i++) {
+        for (int j = primes[i]; j <= x; j++) {
+          ways[j] += ways[j - primes[i]];
+        }
       }
+      if (ways[x] > 5000) { break; }
+      x++;
     }
-
-    return "0";
+    return String.valueOf(x);
   }
 }
--- src/test/java/ssta/pe/p71_p80/Problem77Test.java
+++ src/test/java/ssta/pe/p71_p80/Problem77Test.java
@@ -1,11 +1,16 @@
 package ssta.pe.p71_p80;
 
 import static org.junit.Assert.*;
+import org.junit.Test;
 
 /**
  * ssta.pe.p71_p80.Problem77Test, created on 15/01/2016 13:48 <p>
  * @author StephenSt
  */
 public class Problem77Test {
-
+  @Test
+  public void testSolve() throws Exception {
+    String result = new Problem77().solve();
+    assertEquals("71", result);
+  }
 }
