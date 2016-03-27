2b0c393d4f30eb03e40bcf641e0e0e3f993fe884
ssta <clothcat@gmail.com>
2016-03-27 14:39:24 +0100

    * Delete primesBigList

---
+++ src/main/java/ssta/pe/p81_90/Problem89.java
@@ -0,0 +1,14 @@
+package ssta.pe.p81_90;
+
+/**
+ * Created by ssta on 27/03/16.
+ */
+public class Problem89 {
+  public static void main(String[] args) {
+    System.out.println(new Problem89().solve());
+  }
+
+  private String solve() {
+    return null;
+  }
+}
