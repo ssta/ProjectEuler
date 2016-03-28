65f7818780f47c36c90b728df2a4d596f1218c13
ssta <clothcat@gmail.com>
2016-03-28 15:31:54 +0100

    * Problem 89

--- src/main/java/ssta/pe/p81_90/Problem89.java
+++ src/main/java/ssta/pe/p81_90/Problem89.java
@@ -1,14 +1,33 @@
 package ssta.pe.p81_90;
 
-/**
- * Created by ssta on 27/03/16.
- */
+import java.util.Scanner;
+
 public class Problem89 {
   public static void main(String[] args) {
     System.out.println(new Problem89().solve());
   }
 
   private String solve() {
-    return null;
+    int inputLength = 0;
+    int outputLength = 0;
+    // read the file into a string
+    try (Scanner sc = new Scanner(this.getClass().getResourceAsStream("/p089_roman.txt"))) {
+      StringBuilder sb = new StringBuilder();
+      while (sc.hasNextLine()) {
+        sb.append(sc.nextLine()).append("\n");
+      }
+      String input = sb.toString();
+      // replace anything that can get shorter into a new string
+      String pattern = "DCCCC|LXXXX|VIIII|CCCC|XXXX|IIII";
+
+      // all of the patterns can be expresseed with two characters (doesn't matter what they are, just that there are two of them
+      String output = input.replaceAll(pattern, "YY");
+
+      // compare the string sizes
+      inputLength = input.length();
+      outputLength = output.length();
+    }
+    return String.valueOf(inputLength - outputLength);
   }
 }
+
